ba1a7be72f067dc45f9655a569a45db4d31e9f40
Isa Gul <isa.gul@beyn.com.tr>
2020-06-01 13:05:28 +0300

    loading was added while creating scrum planning

--- src/components/AddStoryList/index.js
+++ src/components/AddStoryList/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import App from '../../App';
-import { Input, Button } from 'antd';
+import { Input, Button, Spin } from 'antd';
 import { useFormik } from 'formik';
 import { useHistory } from "react-router-dom";
 import { openNotificationWithIcon } from '../../utils';
@@ -11,6 +11,7 @@ import './index.scss';
 
 const AddStoryList = (props) => {
     let history = useHistory();
+    const [isPlanCreated, setIsPlanCreated] = useState(false);
     const { TextArea } = Input;
 
     const formik = useFormik({
@@ -31,6 +32,7 @@ const AddStoryList = (props) => {
                 .required('required'),
         }),
         onSubmit: values => {
+            setIsPlanCreated(true);
             fetch(`${apiPrefix}/session/create`, {
                 method: 'POST',
                 headers: {
@@ -50,6 +52,7 @@ const AddStoryList = (props) => {
                             pathname: `${ROUTER.viewScrumMaster}/${values.sessionName}`,
                             state: { sessionName: values.sessionName }
                         });
+                        setIsPlanCreated(false);
                     } else {
                         openNotificationWithIcon('error', `${response.message}`)
                     }
@@ -59,49 +62,52 @@ const AddStoryList = (props) => {
 
     return (
         <App>
-            <div className="add-story-list-component">
-                <form onSubmit={formik.handleSubmit}>
-                    <div className="inputs-area">
-                        <label htmlFor="session-name">Session Name:</label>
-                        <div className="input">
-                            <Input
-                                id="session-name"
-                                name="sessionName"
+            <Spin tip="Scrum planning is being created. Please wait..." spinning={isPlanCreated}>
+                <div className="add-story-list-component">
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="inputs-area">
+                            <label htmlFor="session-name">Session Name:</label>
+                            <div className="input">
+                                <Input
+                                    id="session-name"
+                                    name="sessionName"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.sessionName}
+                                    placeholder="Session Name" />
+                                {formik.touched.sessionName && formik.errors.sessionName ? (
+                                    <span className="invalid">{formik.errors.sessionName}</span>
+                                ) : null}
+                            </div>
+                            <label htmlFor="voters-number">Number of Voters:</label>
+                            <div className="input">
+                                <Input
+                                    id="voters-number"
+                                    type="number"
+                                    name="votersNumber"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.votersNumber}
+                                    placeholder="Number of Voters" />
+                                {formik.touched.votersNumber && formik.errors.votersNumber ? (
+                                    <span className="invalid">{formik.errors.votersNumber}</span>
+                                ) : null}
+                            </div>
+                        </div>
+                        <div className="story-area">
+                            <p>Paste your story list (Each line will be converted as a story)</p>
+                            <TextArea
                                 onChange={formik.handleChange}
-                                value={formik.values.sessionName}
-                                placeholder="Session Name" />
-                            {formik.touched.sessionName && formik.errors.sessionName ? (
-                                <span className="invalid">{formik.errors.sessionName}</span>
+                                value={formik.values.stories}
+                                name="stories"
+                                rows={10} />
+                            {formik.touched.stories && formik.errors.stories ? (
+                                <span className="invalid">{formik.errors.stories}</span>
                             ) : null}
                         </div>
-                        <label htmlFor="voters-number">Number of Voters:</label>
-                        <div className="input">
-                            <Input
-                                id="voters-number"
-                                type="number"
-                                name="votersNumber"
-                                onChange={formik.handleChange}
-                                value={formik.values.votersNumber}
-                                placeholder="Number of Voters" />
-                            {formik.touched.votersNumber && formik.errors.votersNumber ? (
-                                <span className="invalid">{formik.errors.votersNumber}</span>
-                            ) : null}
-                        </div>
-                    </div>
-                    <div className="story-area">
-                        <p>Paste your story list (Each line will be converted as a story)</p>
-                        <TextArea
-                            onChange={formik.handleChange}
-                            value={formik.values.stories}
-                            name="stories"
-                            rows={10} />
-                        {formik.touched.stories && formik.errors.stories ? (
-                            <span className="invalid">{formik.errors.stories}</span>
-                        ) : null}
-                    </div>
-                    <Button htmlType="submit" className="start-session">Start Session</Button>
-                </form>
-            </div>
+                        <Button htmlType="submit" className="start-session">Start Session</Button>
+                    </form>
+                </div>
+            </Spin>
+
         </App>
     )
 }
